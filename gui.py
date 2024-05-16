4c4f642982dfdb4a882b2a08e4ead2064494cd48
Antoine Vincent Martin <martantoine@tuta.io>
2024-05-16 11:05:01 +0200

    jog mode

--- gui.py
+++ gui.py
@@ -15,16 +15,17 @@ import os
 
 COMMAND_BYTES_LEN = 4
 OBSERVATION_BYTES_LEN = 24
+VALVES_HISTORY_WIDTH = 400
 onnx_path = "remote_model.onnx"
+drawlists = []
 
 max_history_time_seconds = 10
 update_interval = 0.5
 valve_history_length = int(max_history_time_seconds / update_interval)
-r = [[0 for i in range(valve_history_length)] for j in range(10)]
 valves = [deque([False]*valve_history_length, maxlen=valve_history_length) for i in range(10)]
 pressures = [deque([0]*valve_history_length, maxlen=valve_history_length) for i in range(2)]
 xP = list(np.linspace(0, max_history_time_seconds, valve_history_length))
-w = 20
+w = VALVES_HISTORY_WIDTH / (valve_history_length)
 
 serial_order = 0
 
@@ -108,22 +109,49 @@ def start_communicate():
     thread = threading.Thread(name="communication", target=communicate, args=(), daemon=True)
     thread.start()
 
+def history_resize():
+    global valve_history_length, w, drawlists, valves, pressures, xP
+    for y in range(10):
+        for x in range(valve_history_length):
+            dpg.delete_item(item=1000 + x + y*valve_history_length)
+    valve_history_length = int(dpg.get_value(item=300) / update_interval)
+    for i in range(10):
+        valves[i] = deque([0]*valve_history_length, maxlen=valve_history_length)
+    pressures[0] = deque([0]*valve_history_length, maxlen=valve_history_length)
+    pressures[1] = deque([0]*valve_history_length, maxlen=valve_history_length)
+    xP = list(np.linspace(0, max_history_time_seconds, valve_history_length))
+
+    w = VALVES_HISTORY_WIDTH / (valve_history_length)
+    for y in range(10):
+        for x in range(valve_history_length):
+            dw = 0
+            if valves[y][x] == 0:
+                color = [0xff, 0, 0, 0 + 0xff * x / valve_history_length]
+            else:
+                color = [0x11, 0x11, 0x11, 0 + 0xff * x / valve_history_length]
+            if x == valve_history_length - 1:
+                dw = w
+            dpg.draw_rectangle(pmin=[x*w, 0], pmax=[(x+0.9)*w + dw, 0.9*20], color=color, fill=color, rounding=1.0, tag=1000 + x + y * valve_history_length, parent=drawlists[y])
+                        
 def main():
+    global drawlists
     dpg.create_context()
-    dpg.create_viewport()
-    dpg.show_imgui_demo()
-    with dpg.window(label="Main"):
+    dpg.create_viewport(decorated=False, resizable=False, width=500, height=800)
+    
+    with dpg.window(label="Main", no_title_bar=True, no_resize=True, no_collapse=True, no_move=True, no_background=False, no_scrollbar=True, width=500, height=800):
         with dpg.collapsing_header(label="Communication", default_open=True):
-            #dpg.add_input_text(label="Serial port", width=120, default_value=sys.argv[1], callback=communicate)
             dpg.add_separator()
             with dpg.group(horizontal=True):
                 dpg.add_button(label="Disonnect", callback=close_serial, tag=100)
                 dpg.add_text("Status: Unknown", tag=200)    
+                
+        with dpg.collapsing_header(label="Valves Commands Histograph", default_open=True):
+            dpg.add_slider_int(label="History length (seconds)", default_value=valve_history_length, min_value=10, width=300, max_value=20, tag=300, callback=history_resize)
 
-        with dpg.collapsing_header(label="Valves Histograph", default_open=True):
             dpg.add_text("On/Off valves commands")
             for y in range(10):
-                with dpg.drawlist((valve_history_length+5)*w, w):
+                with dpg.drawlist((valve_history_length+5)*w, w) as di:
+                    drawlists.append(di)
                     for x in range(valve_history_length):
                         dw = 0
                         if valves[y][x] == 0:
@@ -132,27 +160,47 @@ def main():
                             color = [0x11, 0x11, 0x11, 0 + 0xff * x / valve_history_length]
                         if x == valve_history_length - 1:
                             dw = w
-                        r[y][x] = dpg.draw_rectangle(pmin=[x*w, 0], pmax=[(x+0.9)*w + dw, 0.9*w], color=color, fill=color, rounding=1.0, tag=1000 + x + y * valve_history_length)
-                        dpg.draw_text([(valve_history_length+1.5)*w, 0], "valve %d" % y, size=15.0, color=color)
+                        dpg.draw_rectangle(pmin=[x*w, 0], pmax=[(x+0.9)*w + dw, 0.9*w], color=color, fill=color, rounding=1.0, tag=1000 + x + y * valve_history_length, parent=drawlists[-1])
+                        dpg.draw_text([VALVES_HISTORY_WIDTH+1.5*w, 0], "valve %d" % y, size=15.0, color=color)
                         
         
             with dpg.plot(label="Pressure commands", height=200, width=(valve_history_length+5)*w):
-                # optionally create legend
                 dpg.add_plot_legend()
 
-                # REQUIRED: create x and y axes
                 dpg.add_plot_axis(dpg.mvXAxis, label="time", tag="x_axis")
                 yax = dpg.add_plot_axis(dpg.mvYAxis, label="pressure", tag="y_axis")
-                
-                # series belong to a y axis
+
                 pressures_plot = [0]*2
                 pressures_plot[0] = dpg.add_stair_series(list(xP), list(pressures[0]), label="hip", parent="y_axis")
                 pressures_plot[1] = dpg.add_stair_series(list(xP), list(pressures[1]), label="knee", parent="y_axis")
-                
+
+        with dpg.collapsing_header(label="Jog Mode", default_open=True):
+            with dpg.group(horizontal=True, horizontal_spacing=200):
+                with dpg.group(horizontal=False):
+                    dpg.add_text("Hip Joints")
+                    with dpg.group(horizontal=True):
+                        dpg.add_slider_int(width=60, default_value=0, min_value=0, max_value=5, vertical=True, format="%d bar")
+                        with dpg.group(horizontal=False):
+                            dpg.add_checkbox(label="Valve 0", default_value=True)
+                            dpg.add_checkbox(label="Valve 1", default_value=True)
+                            dpg.add_checkbox(label="Valve 2", default_value=True)
+                            dpg.add_checkbox(label="Valve 3", default_value=True)
+                            dpg.add_checkbox(label="Valve 4", default_value=True)
+                with dpg.group(horizontal=False):
+                    dpg.add_text("Knee Joints")
+                    with dpg.group(horizontal=True):
+                        dpg.add_slider_int(width=60, default_value=0, min_value=0, max_value=5, vertical=True, format="%d bar")
+                        with dpg.group(horizontal=False):
+                            dpg.add_checkbox(label="Valve 5", default_value=True)
+                            dpg.add_checkbox(label="Valve 6", default_value=True)
+                            dpg.add_checkbox(label="Valve 7", default_value=True)
+                            dpg.add_checkbox(label="Valve 8", default_value=True)
+                            dpg.add_checkbox(label="Valve 9", default_value=True)
+            dpg.add_button(label="Send Command", width=500, height=50)
     thread1 = threading.Thread(name="update plots", target=update_plots, args=((pressures_plot, yax, )), daemon=True)
     thread1.start()
     start_communicate()
-    
+
     dpg.setup_dearpygui()
     dpg.show_viewport()
     dpg.start_dearpygui()
